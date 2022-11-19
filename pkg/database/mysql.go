0bf9f27d7ab9780c8b419e6d4205361426fe593c
Yudi Prayoga <yudiprayoga26@ubuntu22.10>
2022-11-19 23:08:37 +0700

    [#2] feat: add database connection

--- pkg/database/mysql.go
+++ pkg/database/mysql.go
@@ -1,1 +1,26 @@
 package database
+
+import (
+	"fmt"
+
+	"gorm.io/driver/mysql"
+	"gorm.io/gorm"
+)
+
+var DB *gorm.DB
+
+func InitDatabase(dbHost string, dbPort string, dbUsername string, dbPassword string, dbName string) (*gorm.DB, error) {
+	var err error
+
+	dsn := fmt.Sprintf("%s:%s@tcp(%s:%s)/%s?charset=utf8mb4&parseTime=True&loc=Local", dbUsername, dbPassword, dbHost, dbPort, dbName)
+
+	DB, err = gorm.Open(mysql.Open(dsn), &gorm.Config{})
+
+	if err != nil {
+		panic(err.Error())
+	}
+
+	fmt.Println("Database Connected!")
+
+	return DB, err
+}

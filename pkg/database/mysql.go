532ed8ecc904060ed8885974628a13fa5b64d81e
suryaadi44 <suryawarior44@gmail.com>
2022-11-22 19:16:10 +0800

    [#2] remove unnecessary global var

--- pkg/database/mysql.go
+++ pkg/database/mysql.go
@@ -7,20 +7,13 @@ import (
 	"gorm.io/gorm"
 )
 
-var DB *gorm.DB
-
 func InitDatabase(dbHost string, dbPort string, dbUsername string, dbPassword string, dbName string) (*gorm.DB, error) {
-	var err error
-
 	dsn := fmt.Sprintf("%s:%s@tcp(%s:%s)/%s?charset=utf8mb4&parseTime=True&loc=Local", dbUsername, dbPassword, dbHost, dbPort, dbName)
 
-	DB, err = gorm.Open(mysql.Open(dsn), &gorm.Config{})
-
+	db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
 	if err != nil {
 		panic(err.Error())
 	}
 
-	fmt.Println("Database Connected!")
-
-	return DB, err
+	return db, err
 }

22dae430363e90f089ff6630d049576326cb7c4b
suryaadi44 <suryawarior44@gmail.com>
2022-11-24 00:44:13 +0800

    [#16] test: add test for login controller

--- internal/auth/controller/auth_controller_test.go
+++ internal/auth/controller/auth_controller_test.go
@@ -106,7 +106,96 @@ func (s *TestSuiteAuthController) TestRegisterUser() {
 			resp, err := s.fiberApp.Test(r)
 			s.NoError(err)
 
-			var body fiber.Map
+			var body response.BaseResponse
+			err = json.NewDecoder(resp.Body).Decode(&body)
+			s.NoError(err)
+
+			s.Equal(tc.ExpectedStatus, resp.StatusCode)
+			s.Equal(tc.ExpectedBody, body)
+		})
+		s.TearDownTest()
+	}
+}
+
+func (s *TestSuiteAuthController) TestLoginUser() {
+	loginReq := dto.LoginRequest{
+		Email:    "mail@mail.com",
+		Password: "password",
+	}
+
+	token := &dto.TokenPair{
+		AccessToken:  "some_access_token",
+		RefreshToken: "some_refresh_token",
+	}
+
+	for _, tc := range []struct {
+		Name           string
+		MimeType       string
+		RequestBody    interface{}
+		ServiceReturn  *dto.TokenPair
+		ServiceErr     error
+		ExpectedStatus int
+		ExpectedBody   response.BaseResponse
+		ExpectedErr    error
+	}{
+		{
+			Name:           "Success logging in user",
+			MimeType:       fiber.MIMEApplicationJSON,
+			RequestBody:    loginReq,
+			ServiceReturn:  token,
+			ExpectedStatus: fiber.StatusOK,
+			ExpectedBody: response.BaseResponse{
+				Message: "user logged in successfully",
+				Data: map[string]interface{}{
+					"accessToken":  token.AccessToken,
+					"refreshToken": token.RefreshToken,
+				},
+			},
+		},
+		{
+			Name:           "Failed logging in user: invalid request body",
+			MimeType:       fiber.MIMETextPlain,
+			RequestBody:    []byte("invalid request body"),
+			ExpectedStatus: fiber.StatusBadRequest,
+			ExpectedBody: response.BaseResponse{
+				Message: err2.ErrInvalidRequestBody.Error(),
+			},
+		},
+		{
+			Name:           "Failed logging in user: invalid credentials",
+			MimeType:       fiber.MIMEApplicationJSON,
+			RequestBody:    loginReq,
+			ServiceErr:     err2.ErrInvalidCredentials,
+			ExpectedStatus: fiber.StatusUnauthorized,
+			ExpectedBody: response.BaseResponse{
+				Message: err2.ErrInvalidCredentials.Error(),
+			},
+		},
+		{
+			Name:           "Failed logging in user: service error",
+			MimeType:       fiber.MIMEApplicationJSON,
+			RequestBody:    loginReq,
+			ServiceErr:     errors.New("error"),
+			ExpectedStatus: fiber.StatusInternalServerError,
+			ExpectedBody: response.BaseResponse{
+				Message: "error",
+			},
+		},
+	} {
+		s.SetupTest()
+		s.Run(tc.Name, func() {
+			jsonBody, err := json.Marshal(tc.RequestBody)
+			s.NoError(err)
+
+			s.mockAuthService.On("LoginUser", mock.Anything, mock.Anything).Return(tc.ServiceReturn, tc.ServiceErr)
+
+			s.fiberApp.Post("/", s.authController.LoginUser)
+			r := httptest.NewRequest("POST", "/", bytes.NewBuffer(jsonBody))
+			r.Header.Set(fiber.HeaderContentType, tc.MimeType)
+			resp, err := s.fiberApp.Test(r)
+			s.NoError(err)
+
+			var body response.BaseResponse
 			err = json.NewDecoder(resp.Body).Decode(&body)
 			s.NoError(err)
 

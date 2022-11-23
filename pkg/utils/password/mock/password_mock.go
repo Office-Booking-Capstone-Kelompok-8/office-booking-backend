d55487db0d69c7887247430f240a7114ead439df
suryaadi44 <suryawarior44@gmail.com>
2022-11-23 11:45:28 +0800

    [#15] test: add test for auth service

--- pkg/utils/password/mock/password_mock.go
+++ pkg/utils/password/mock/password_mock.go
@@ -2,16 +2,16 @@ package mock
 
 import "github.com/stretchr/testify/mock"
 
-type MockPasswordHashFunction struct {
+type PasswordHashService struct {
 	mock.Mock
 }
 
-func (m *MockPasswordHashFunction) GenerateFromPassword(password []byte, cost int) ([]byte, error) {
+func (m *PasswordHashService) GenerateFromPassword(password []byte, cost int) ([]byte, error) {
 	args := m.Called(password, cost)
 	return args.Get(0).([]byte), args.Error(1)
 }
 
-func (m *MockPasswordHashFunction) CompareHashAndPassword(hashedPassword, password []byte) error {
+func (m *PasswordHashService) CompareHashAndPassword(hashedPassword, password []byte) error {
 	args := m.Called(hashedPassword, password)
 	return args.Error(0)
 }

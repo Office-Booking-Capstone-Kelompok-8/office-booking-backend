36dd68333b1f5fda15e77899e6ffd6e109735f10
suryaadi44 <suryawarior44@gmail.com>
2022-11-25 04:04:19 +0800

    [#41] test: add auth repo test for change password

--- internal/auth/repository/impl/auth_repository_impl_test.go
+++ internal/auth/repository/impl/auth_repository_impl_test.go
@@ -132,6 +132,45 @@ func (s *TestSuiteAuthRepository) TestGetFullUserByEmail() {
 	}
 }
 
+func (s *TestSuiteAuthRepository) TestGetUserByEmail() {
+	query := regexp.QuoteMeta("SELECT * FROM `users` WHERE email = ? AND `users`.`deleted_at` IS NULL ORDER BY `users`.`id` LIMIT 1")
+	for _, tc := range []struct {
+		Name        string
+		Err         error
+		ExpectedErr error
+	}{
+		{
+			Name:        "Success",
+			Err:         nil,
+			ExpectedErr: nil,
+		},
+		{
+			Name:        "Error: no record found",
+			Err:         gorm.ErrRecordNotFound,
+			ExpectedErr: err2.ErrUserNotFound,
+		},
+		{
+			Name:        "Error: unknown",
+			Err:         errors.New("unknown error"),
+			ExpectedErr: errors.New("unknown error"),
+		},
+	} {
+		s.SetupTest()
+		s.Run(tc.Name, func() {
+			if tc.Err != nil {
+				s.mock.ExpectQuery(query).WillReturnError(tc.Err)
+			} else {
+				s.mock.ExpectQuery(query).WillReturnRows(sqlmock.NewRows([]string{"id", "username", "password", "role"}).AddRow(1, "123", "123", 1))
+			}
+
+			_, err := s.repo.GetUserByEmail(context.Background(), "123")
+
+			s.Equal(tc.ExpectedErr, err)
+		})
+		s.TearDownTest()
+	}
+}
+
 func (s *TestSuiteAuthRepository) TestGetFullUserByID() {
 	query := regexp.QuoteMeta("SELECT `users`.`id`,`users`.`email`,`users`.`password`,`users`.`role`,`users`.`is_verified`,`users`.`created_at`,`users`.`updated_at`,`users`.`deleted_at`,`Detail`.`user_id` AS `Detail__user_id`,`Detail`.`name` AS `Detail__name`,`Detail`.`phone` AS `Detail__phone`,`Detail`.`picture_id` AS `Detail__picture_id`,`Detail`.`created_at` AS `Detail__created_at`,`Detail`.`updated_at` AS `Detail__updated_at`,`Detail`.`deleted_at` AS `Detail__deleted_at` FROM `users` LEFT JOIN `user_details` `Detail` ON `users`.`id` = `Detail`.`user_id` AND `Detail`.`deleted_at` IS NULL WHERE id = ? AND `users`.`deleted_at` IS NULL ORDER BY `users`.`id` LIMIT 1")
 	for _, tc := range []struct {
@@ -164,6 +203,49 @@ func (s *TestSuiteAuthRepository) TestGetFullUserByID() {
 			}
 
 			_, err := s.repo.GetFullUserByID(context.Background(), "123")
+
+			s.Equal(tc.ExpectedErr, err)
+		})
+		s.TearDownTest()
+	}
+}
+
+func (s *TestSuiteAuthRepository) TestChangePassword() {
+	query := regexp.QuoteMeta("UPDATE `users` SET `password`=?,`updated_at`=? WHERE id = ? AND `users`.`deleted_at` IS NULL")
+	for _, tc := range []struct {
+		Name         string
+		Err          error
+		ExpectedErr  error
+		RowsAffected int64
+	}{
+		{
+			Name:         "Success",
+			Err:          nil,
+			ExpectedErr:  nil,
+			RowsAffected: 1,
+		},
+		{
+			Name:         "Error no record found",
+			Err:          nil,
+			ExpectedErr:  err2.ErrUserNotFound,
+			RowsAffected: 0,
+		},
+		{
+			Name:         "Error: unknown",
+			Err:          errors.New("unknown error"),
+			ExpectedErr:  errors.New("unknown error"),
+			RowsAffected: 0,
+		},
+	} {
+		s.SetupTest()
+		s.Run(tc.Name, func() {
+			if tc.Err != nil {
+				s.mock.ExpectExec(query).WillReturnError(tc.Err)
+			} else {
+				s.mock.ExpectExec(query).WillReturnResult(sqlmock.NewResult(1, tc.RowsAffected))
+			}
+
+			err := s.repo.ChangePassword(context.Background(), "123", "123")
 
 			s.Equal(tc.ExpectedErr, err)
 		})

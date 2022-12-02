f7809c3d6e41c0af9e6a57f1c4df6693214b1b58
suryaadi44 <suryawarior44@gmail.com>
2022-12-02 11:44:14 +0800

    [#84] feat: delete building

--- internal/reservation/repository/reservation_repository.go
+++ internal/reservation/repository/reservation_repository.go
@@ -4,4 +4,5 @@ import "golang.org/x/net/context"
 
 type ReservationRepository interface {
 	CountUserActiveReservations(ctx context.Context, userID string) (int64, error)
+	CountBuildingActiveReservations(ctx context.Context, buildingID string) (int64, error)
 }

d7d39cf33a55cf495a9ecd9b0a11118da6018663
Surya Adi <suryawarior44@gmail.com>
2022-12-15 04:46:10 +0800

    feat: get reservation payment detail (admin) (#179)

--- internal/payment/repository/payment_repository.go
+++ internal/payment/repository/payment_repository.go
@@ -9,7 +9,7 @@ type PaymentRepository interface {
 	GetAllPaymentMethod(ctx context.Context) (*entity.Payments, error)
 	GetAllBank(ctx context.Context) (*entity.Banks, error)
 	GetPaymentMethodByID(ctx context.Context, paymentID int) (*entity.Payment, error)
-	GetReservationPaymentByID(ctx context.Context, reservationID string) (*entity.Transaction, error)
+	GetReservationPaymentByID(ctx context.Context, reservationID string, userID string) (*entity.Transaction, error)
 	CreatePaymentMethod(ctx context.Context, payment *entity.Payment) error
 	CreateNewReservationPayment(ctx context.Context, payment *entity.Transaction) error
 	UpdatePaymentMethod(ctx context.Context, payment *entity.Payment) error
